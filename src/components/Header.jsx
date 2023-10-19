348eba30b1d67d39ac8eef0f94db9fb4b32183aa
Jenya <113167825+Genya10@users.noreply.github.com>
2023-10-19 19:06:56 +0300

    change active class

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import cl from '../classes/Header.module.css';
-import {FaShoppingCart} from 'react-icons/fa'
+import {FaShoppingCart} from 'react-icons/fa';
+import { useState } from "react";
 
 export const Header=()=>{
+    let [cartOpen,setCartOpen]=useState(false);
+    let [active,setActive]=useState(false)
     return(
         <header>
             <div>
@@ -13,7 +16,11 @@ export const Header=()=>{
                 <li>Contacts</li>
                 <li>Cabinet</li>
             </ul>
-            <FaShoppingCart className={cl.shopButton}/>
+            <FaShoppingCart onClick={()=>setCartOpen(cartOpen=!cartOpen)}
+                            className={` ${cl.shopButton} ${cartOpen && cl.active} `}/>
+                            {cartOpen && (
+                                <div className={cl.shopCart}></div>
+                            )}
             <div className={cl.presentation}>           
             </div>
         </header>
