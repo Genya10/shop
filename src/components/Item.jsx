115cc88380648b9f83dcafdb5ee1eb57a6239d59
Jenya <113167825+Genya10@users.noreply.github.com>
2023-10-19 17:13:24 +0300

    add a little styles

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import cl from '../classes/Item.module.css'
 
 export const Item =(props)=>{
-    return(<div className={cl.item}>
-        <h3>{props.item.title}</h3>
+    return(<div className={cl.item}>       
         <img src={"./img/"+props.item.img  }/>
+        <h3>{props.item.title}</h3>
         <p>{props.item.desc}</p>
         <b>{props.item.price}$</b>
         <div className={cl.addToCart}>+</div>
